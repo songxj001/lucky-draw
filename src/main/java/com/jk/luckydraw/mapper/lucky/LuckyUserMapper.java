84ed915b66d92d951b83cb38fe49c1d1097f1f37
songxuejun <sxjworld@126.com>
2018-11-27 11:56:51 +0800

    新增ip地址记录

--- src/main/java/com/jk/luckydraw/mapper/lucky/LuckyUserMapper.java
+++ src/main/java/com/jk/luckydraw/mapper/lucky/LuckyUserMapper.java
@@ -9,6 +9,6 @@ public interface LuckyUserMapper {
     @Select("select count(1) from t_lucky_user where name = #{name} or phoneNumber = #{phoneNumber} or mac = #{mac}")
     int checkUser(LuckyUserBean luckyUserBean);
 
-    @Insert("insert into t_lucky_user(name,sex,photo,phoneNumber,createTime,mac) values(#{name},#{sex},#{photo},#{phoneNumber},now(),#{mac})")
+    @Insert("insert into t_lucky_user(name,sex,photo,phoneNumber,createTime,mac,ip) values(#{name},#{sex},#{photo},#{phoneNumber},now(),#{mac},#{ip})")
     void saveLuckyUser(LuckyUserBean luckyUserBean);
 }
